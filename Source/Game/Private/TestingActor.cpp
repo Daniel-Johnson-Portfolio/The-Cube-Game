d0a3d2c3f0aab77da6edf42d88f0ee99b4df3507
Daniel-Johnson-Portfolio <dannyj0207@gmail.com>
2024-09-30 17:53:28 +0100

    Arrow

---
+++ Source/Game/Private/TestingActor.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestingActor.h"
+
+#include "MovieSceneObjectBindingID.h"
+#include "Preferences/PersonaOptions.h"
+
+
+// Sets default values
+ATestingActor::ATestingActor()
+{
+	_Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = _Root;
+
+	_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	_Mesh -> SetupAttachment(_Root);
+
+	_Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	_Arrow -> SetupAttachment(_Root);
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATestingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Game/Private/TestingActor.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/Actor.h"
+#include "Components/ArrowComponent.h"
+#include "TestingActor.generated.h"
+
+UCLASS(Abstract)
+class GAME_API ATestingActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATestingActor();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USceneComponent> _Root;
+	UPROPERTY(EditAnywhere, BlueprintreadWrite)
+	TObjectPtr<UStaticMeshComponent> _Mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UArrowComponent> _Arrow;
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
