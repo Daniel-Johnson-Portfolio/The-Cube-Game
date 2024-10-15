3fcc133724017388a19d9229a1b38b05016dc2ee
Daniel-Johnson-Portfolio <dannyj0207@gmail.com>
2024-10-15 17:30:35 +0100

    WeaponBase

---
+++ Source/Game/Private/Weapon_Base.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon_Base.h"
+
+
+// Sets default values
+AWeapon_Base::AWeapon_Base()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+
---
+++ Source/Game/Public/Weapon_Base.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon_Base.generated.h"
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWeaponFireSignature);
+
+UCLASS(Abstract)
+class GAME_API AWeapon_Base : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeapon_Base();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
