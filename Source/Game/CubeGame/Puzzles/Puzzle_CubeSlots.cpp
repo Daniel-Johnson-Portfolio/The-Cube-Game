f85ed4038e73f72b9781fda1935c4cd1f19b7ccf
Daniel-Johnson-Portfolio <dannyj0207@gmail.com>
2024-11-11 22:12:42 +0000

    Puzzle2

---
+++ Source/Game/CubeGame/Puzzles/Puzzle_CubeSlots.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Puzzle_CubeSlots.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+APuzzle_CubeSlots::APuzzle_CubeSlots()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	_BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	
+}
+
+// Called when the game starts or when spawned
+void APuzzle_CubeSlots::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APuzzle_CubeSlots::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Game/CubeGame/Puzzles/Puzzle_CubeSlots.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Puzzle_CubeSlots.generated.h"
+
+class UBoxComponent;
+class UCubeType;
+
+UCLASS()
+class GAME_API APuzzle_CubeSlots : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APuzzle_CubeSlots();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Puzzle")
+	UCubeType* _CubeType;
+
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UBoxComponent> _BoxComponent;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
