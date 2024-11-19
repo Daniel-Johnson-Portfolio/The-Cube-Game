d5bb5a962040c29fe4255b71204916522379ba36
Daniel-Johnson-Portfolio <dannyj0207@gmail.com>
2024-11-19 18:13:41 +0000

    Full BT

--- Source/Game/CubeGame/BTTask_HasReachedPlayer.cpp
+++ Source/Game/CubeGame/BTTask_HasReachedPlayer.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BTTask_FollowPlayer.h"
+#include "BTTask_HasReachedPlayer.h"
 
 #include "AIController.h"
 #include "AiControllerInterface.h"
@@ -9,39 +9,19 @@
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 
-EBTNodeResult::Type UBTTask_FollowPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+EBTNodeResult::Type UBTTask_HasReachedPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	if(UKismetSystemLibrary::DoesImplementInterface(OwnerComp.GetAIOwner(), UAiControllerInterface::StaticClass()))
 	{
 		_AiController = OwnerComp.GetAIOwner();
 		_CurrentPlayer = IAiControllerInterface::Execute_newPawn(_AiController);
-
 		if(UKismetSystemLibrary::DoesImplementInterface(_AiController->GetPawn(), UPawnInterface::StaticClass()))
 		{
 			_AIPawn = IPawnInterface::Execute_GetPawn(_AiController->GetPawn());
 		}
-		
 		FVector AiPawnLocation = IPawnInterface::Execute_GetPawnLocation(_AIPawn);
 		FVector CurrentPlayerLocation = IPawnInterface::Execute_GetPawnLocation(_CurrentPlayer);
-		
-		bInRange = OwnerComp.GetBlackboardComponent()->GetValueAsBool(TEXT("IsInRange"));
-		
-		if(!bInRange)
-		{
-			FVector NewLocation = FMath::VInterpTo(AiPawnLocation, FVector(CurrentPlayerLocation.X, CurrentPlayerLocation.Y, CurrentPlayerLocation.Z / 2.0f), GetWorld()->GetDeltaSeconds(), 1.0f);
-			IPawnInterface::Execute_SetPawnLocation(_AIPawn, NewLocation);
-			return EBTNodeResult::Failed;
-		}
-		
-		if(OwnerComp.GetBlackboardComponent()->GetValueAsBool(TEXT("Pause")))
-		{
-			_AIPawn->SetActorLocation(_AIPawn->GetActorLocation());
-			return EBTNodeResult::Failed;
-		}
-		
-			return EBTNodeResult::Succeeded;
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool(TEXT("IsInRange"), (FVector::Dist(CurrentPlayerLocation, AiPawnLocation) < 250.0f));
 	}
-	
-	return Super::ExecuteTask(OwnerComp, NodeMemory);
+	return EBTNodeResult::Succeeded;
 }
-
---
+++ Source/Game/CubeGame/BTTask_HasReachedPlayer.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_HasReachedPlayer.generated.h"
+
+class ACubeBase;
+/**
+ * 
+ */
+UCLASS()
+class GAME_API UBTTask_HasReachedPlayer : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	
+	UPROPERTY()
+	AAIController* _AiController;
+
+	UPROPERTY()
+	ACubeBase* _CurrentPlayer;
+
+	UPROPERTY()
+	ACubeBase* _AIPawn;
+};
